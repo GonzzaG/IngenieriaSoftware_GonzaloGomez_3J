f1854558377da790071eecdcbf74756b11f51ad9
Gonzalo Gomez <gonzalogomezhf@gmail.com>
2024-10-24 20:18:15 -0300

    References fixes

--- IngenieriaSoftware.BLL/AuthService.cs
+++ IngenieriaSoftware.BLL/AuthService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IngenieriaSoftware.BEL;
 using IngenieriaSoftware.Servicios;
 
 namespace IngenieriaSoftware.BLL
@@ -14,10 +15,10 @@ namespace IngenieriaSoftware.BLL
             Usuario _Usuario = new Usuario
             {
                 Username = pNombreUsuario,
-                Password = pContrasena
+                _passwordHash = pContrasena
             };
 
-            if (new UsuarioBLL().LogIn(_Usuario.Username, _Usuario.Password))
+            if (new UsuarioBLL().LogIn(_Usuario.Username, _Usuario._passwordHash))
             {
                 SessionManager.LogIn(_Usuario);
 
@@ -41,7 +42,7 @@ namespace IngenieriaSoftware.BLL
             Usuario _Usuario = new Usuario
             {
                 Username = pNombreUsuario,
-                Password = pContrasena
+                _passwordHash = pContrasena
             };
 
             if (new UsuarioBLL().RegistrarUsuario(_Usuario, DateTime.Now))
--- IngenieriaSoftware.DAL/UsuarioDAL.cs
+++ IngenieriaSoftware.DAL/UsuarioDAL.cs
@@ -1,5 +1,4 @@
 ﻿using IngenieriaSoftware.BEL;
-using IngenieriaSoftware.Servicios;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -24,13 +23,13 @@ namespace IngenieriaSoftware.DAL
         }
 
         // Método para obtener un usuario por su nombre
-        public Servicios.Usuario ObtenerUsuarioPorNombre(string pUsuarioNombre)
+        public Usuario ObtenerUsuarioPorNombre(string pUsuarioNombre)
         {
             string query = $"SELECT * FROM usuarios WHERE UserName = '{pUsuarioNombre}'";
             DataSet mDs = new DAO().ExecuteDataSet(query);
             if (mDs.Tables.Count > 0 && mDs.Tables[0].Rows.Count > 0)
             {
-                Servicios.Usuario mUsuario = new Servicios.Usuario();
+                Usuario mUsuario = new Usuario();
                 ValorizarEntidad(mUsuario, mDs.Tables[0].Rows[0]);
                 return mUsuario;
             }
@@ -40,18 +39,18 @@ namespace IngenieriaSoftware.DAL
             }
         }
 
-        public int GuardarUsuario(Servicios.Usuario pUsuario, DateTime FechaInicio)
+        public int GuardarUsuario(Usuario pUsuario, DateTime FechaInicio)
         {
             pUsuario.Id = ProximoId();  
             string query = $"INSERT INTO usuarios (id_usuario, Username, PasswordHash, FechaCreacion) " +
-                           $"VALUES ({pUsuario.Id},'{pUsuario.Username}', '{pUsuario.Password}', '{FechaInicio}')";
+                           $"VALUES ({pUsuario.Id},'{pUsuario.Username}', '{pUsuario._passwordHash}', '{FechaInicio}')";
             return _dao.ExecuteNonQuery(query); // Devuelve 1 si la inserción fue exitosa
         }
 
-        private static void ValorizarEntidad(Servicios.Usuario pUsuario, DataRow pDr)
+        private static void ValorizarEntidad(Usuario pUsuario, DataRow pDr)
         {
             pUsuario.Username = pDr["Username"].ToString();
-            pUsuario.AsignarPassword(pDr["PasswordHash"].ToString());
+            pUsuario._passwordHash = (pDr["PasswordHash"].ToString());
             pUsuario.Id = (int)pDr["Id_usuario"];
 
         }
